484e93cff7d42c4a83cb2aa83837a93f0db652e9
Nguyễn Tấn Phát <kaiser97@Kaiser97.local>
2022-11-05 18:23:41 +0700

    add handlebars-hbs

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const hbs = require('express-handlebars')
+const morgan = require('morgan')
+const app = express()
+const port = 3000
+const path = require('path')
+
+// morgan : HTTP logger 
+app.use(morgan('combined'))
+
+// express handlebars
+app.engine('hbs', hbs.engine({
+  extname:'.hbs' 
+}))
+app.set('view engine', 'hbs');
+app.set('views', './src/resources/views')
+
+// route : tuyen duong 
+app.get('/home', (req, res) => {
+  return res.render('home');
+})
+
+app.get('/news', (req, res) => {
+  return res.render('news');
+})
+
+// 127.0.0.1 - localhost
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
